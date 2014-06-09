ab7787fcd6755b3a2c6f89ac33b8426d74fab535
Petr Hracek <phracek@redhat.com>
2014-06-09 10:20:03 +0200

    Fixes 237 issue.

--- devassistant/gui/__init__.py
+++ devassistant/gui/__init__.py
@@ -1,4 +1,5 @@
 import sys
+import os
 
 from devassistant.gui import main_window
 from devassistant.gui import creator_window
@@ -11,6 +12,9 @@ def run_gui():
         print("%s: %r" % (e.__class__.__name__, str(e)))
         sys.exit(1)
 
+    if not os.environ.get('DISPLAY'):
+        print("Developer Assistant GUI must be run from a graphical environment.")
+        sys.exit(1)
     main_window.MainWindow()
 
 def run_yaml_gui():
