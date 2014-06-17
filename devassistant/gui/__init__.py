934ce7a9aea28ff3f9707e31f62b687a9e09ebb7
Petr Hracek <phracek@redhat.com>
2014-06-17 14:16:15 +0200

    Fixes #235 including test suite

--- devassistant/gui/__init__.py
+++ devassistant/gui/__init__.py
@@ -1,24 +1,41 @@
+import os
 import sys
 
-from devassistant.gui import main_window
-from devassistant.gui import creator_window
+
+GUI_MESSAGE = "Developer Assistant GUI requires a running X server."
+GUI_MESSAGE_DISPLAY = "Environment variable DISPLAY is not set."
+
 
 def run_gui():
+    """
+    Function for running DevAssistant GUI
+    """
     try:
         from gi.repository import Gtk
+    except ImportError as ie:
+        pass
     except RuntimeError as e:
-        print("Developer Assistant GUI requires a running X server.")
+        print(GUI_MESSAGE)
         print("%s: %r" % (e.__class__.__name__, str(e)))
         sys.exit(1)
 
+    if not os.environ.get('DISPLAY'):
+        print("%s %s" % (GUI_MESSAGE, GUI_MESSAGE_DISPLAY))
+        sys.exit(1)
+    from devassistant.gui import main_window
     main_window.MainWindow()
 
+
 def run_yaml_gui():
+    """
+    Function for running DevAssistant YAML Creator GUI
+    """
     try:
         from gi.repository import Gtk
     except RuntimeError as e:
-        print("Developer Assistant GUI requires a running X server.")
+        print(GUI_MESSAGE)
         print("%s: %r" % (e.__class__.__name__, str(e)))
         sys.exit(1)
 
+    from devassistant.gui import creator_window
     creator_window.CreatorWindow()
